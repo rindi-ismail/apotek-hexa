78b1240f9a9febd0d2ecf311373c969f1e2f6b22
Rindi Ismail <rismail@softint.com.au>
2019-11-02 15:36:07 +0700

    005 product migration

---
+++ resources/views/sidemenu.blade.php
@@ -0,0 +1,90 @@
+<ul class="vertical-nav-menu">
+    <li class="app-sidebar__heading">Dashboards</li>
+    <li>
+        <a href="index.html" class="mm-active">
+            <i class="metismenu-icon pe-7s-rocket"></i>
+            Dashboard
+        </a>
+    </li>
+    <li class="app-sidebar__heading">Inventory</li>
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-diamond"></i>
+            Master Data
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li>
+                <a href="elements-buttons-standard.html">
+                    <i class="metismenu-icon"></i>
+                    Category
+                </a>
+            </li>
+            <li>
+                <a href="elements-dropdowns.html">
+                    <i class="metismenu-icon"></i>
+                    Class of Drug
+                </a>
+            </li>
+            <li>
+                <a href="elements-icons.html">
+                    <i class="metismenu-icon"></i>
+                    Product
+                </a>
+            </li>
+            <li>
+                <a href="components-tabs.html">
+                    <i class="metismenu-icon">
+                    </i>Factory
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-car"></i>
+            Logistic
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+
+            <li>
+                <a href="components-accordions.html">
+                    <i class="metismenu-icon">
+                    </i> Product Stock
+                </a>
+            </li>
+            <li>
+                <a href="components-notifications.html">
+                    <i class="metismenu-icon">
+                    </i>Purchase Request
+                </a>
+            </li>
+            <li>
+                <a href="components-modals.html">
+                    <i class="metismenu-icon">
+                    </i>Purchase Order
+                </a>
+            </li>
+            <li>
+                <a href="components-progress-bar.html">
+                    <i class="metismenu-icon">
+                    </i>Goods Receive
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="app-sidebar__heading">Sales</li>
+    <li>
+        <a href="tables-regular.html">
+            <i class="metismenu-icon pe-7s-display2"></i>
+            Price
+        </a>
+    </li>
+    <li>
+        <a href="tables-regular.html">
+            <i class="metismenu-icon pe-7s-display2"></i>
+            Sales
+        </a>
+    </li>
+</ul>
